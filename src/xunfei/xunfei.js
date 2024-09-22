c7e40e5251960aa24ccb291a837e5c793e3e8740
Mengtao Liu <mengtao.liu@shopee.com>
2024-09-22 11:11:30 +0800

    add

--- src/xunfei/xunfei.js
+++ src/xunfei/xunfei.js
@@ -10,9 +10,13 @@ const appID = env.XUNFEI_APP_ID
 const apiKey = env.XUNFEI_API_KEY
 const apiSecret = env.XUNFEI_API_SECRET
 // 地址必须填写，代表着大模型的版本号！！！！！！！！！！！！！！！！
-const httpUrl = new URL('https://spark-api.xf-yun.com/v3.5/chat')
+// const httpUrl = new URL('https://spark-api.xf-yun.com/v3.5/chat')
+const httpUrl = new URL('https://spark-api.xf-yun.com/v4.0/chat')
 
-let modelDomain // V1.1-V3.5动态获取，高于以上版本手动指定
+let histArray = []
+let roleArray = []
+
+let modelDomain = '4.0Ultra' // V1.1-V3.5动态获取，高于以上版本手动指定
 function authenticate() {
   // console.log(httpUrl.pathname)
   // 动态获取domain信息
@@ -35,6 +39,7 @@ function authenticate() {
     let url = 'wss://' + httpUrl.host + httpUrl.pathname
 
     let host = 'localhost:8080'
+    // let host = location.host
     let date = new Date().toGMTString()
     let algorithm = 'hmac-sha256'
     let headers = 'host date request-line'
@@ -54,12 +59,36 @@ export async function xunfeiSendMsg(inputVal) {
   let socket = new WebSocket(String(myUrl))
   let total_res = '' // 请空回答历史
 
+  console.log('inputVal:', inputVal)
   // 创建一个Promise
   let messagePromise = new Promise((resolve, reject) => {
     // 监听websocket的各阶段事件 并做相应处理
     socket.addEventListener('open', (event) => {
       // console.log('socket开启连接', event);
       // 发送消息
+      histArray.push(inputVal)
+      roleArray.push('user')
+      let totalLength = histArray.reduce((sum, currentString) => sum + currentString.length, 0)
+
+      while (totalLength + inputVal.length > 4096) {
+        histArray.shift()
+        histArray.shift()
+        roleArray.shift()
+        roleArray.shift()
+        totalLength = histArray.reduce((sum, currentString) => sum + currentString.length, 0)
+        //sendString = sendString.slice(-2048)
+      }
+      let sendArray = []
+      histArray.forEach((value, index) => {
+        // let role = index % 2 === 0 ? "user" : "assistant"; // 奇数索引为“0”，偶数索引为“1”
+        let role = roleArray[index]
+        let obj = {}
+        obj['role'] = role
+        obj['content'] = value
+        sendArray.push(obj)
+      })
+      console.log('sendArray:', sendArray)
+
       let params = {
         header: {
           app_id: appID,
@@ -76,12 +105,13 @@ export async function xunfeiSendMsg(inputVal) {
           message: {
             // 如果想获取结合上下文的回答，需要开发者每次将历史问答信息一起传给服务端，如下示例
             // 注意：text里面的所有content内容加一起的tokens需要控制在8192以内，开发者如有较长对话需求，需要适当裁剪历史信息
-            text: [
-              { role: 'user', content: '你是谁' }, //# 用户的历史问题
-              { role: 'assistant', content: '你是一个专业的智能助手' }, //# AI的历史回答结果
-              // ....... 省略的历史对话
-              { role: 'user', content: inputVal }, //# 最新的一条问题，如无需上下文，可只传最新一条问题
-            ],
+            //text: [
+            //  // { role: 'user', content: '你是谁' }, //# 用户的历史问题
+            //  // { role: 'assistant', content: '你是一个专业的智能助手' }, //# AI的历史回答结果
+            //  // ....... 省略的历史对话
+            //  { role: 'user', content: sendString}, //# 最新的一条问题，如无需上下文，可只传最新一条问题
+            //],
+            text: sendArray,
           },
         },
       }
@@ -113,6 +143,8 @@ export async function xunfeiSendMsg(inputVal) {
       console.log('socket 连接关闭')
       // 对话完成后socket会关闭，将聊天记录换行处理
       resolve(total_res)
+      histArray.push(total_res)
+      roleArray.push('assistant')
     })
 
     socket.addEventListener('error', (event) => {
